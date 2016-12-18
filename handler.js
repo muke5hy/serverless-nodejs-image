7608899a98709116b99eec505de3d5332dc9caac
Mukesh Yadav <mak.gnu@gmail.com>
2016-12-18 16:01:21 +0530

    Changed serverless config

--- handler.js
+++ handler.js
@@ -1,21 +1,3 @@
-'use strict';
-
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-
 /**
  * Automatic Image resize, reduce with AWS Lambda
  * Lambda main handler
@@ -31,7 +13,7 @@ const fs             = require("fs");
 const path           = require("path");
 
 // Lambda Handler
-exports.handler.cropImage = (event, context) => {
+module.exports.cropImage = (event, context) => {
   const s3Object   = event.Records[0].s3;
   const configPath = path.resolve(__dirname, "config.json");
   const processor  = new ImageProcessor(s3Object);
@@ -39,18 +21,17 @@ exports.handler.cropImage = (event, context) => {
       JSON.parse(fs.readFileSync(configPath, { encoding: "utf8" }))
   );
 
-  processor.run(config)
-      .then((processedImages) => {
+  processor.run(config).then((processedImages) => {
     var message = "OK, " + processedImages + " images were processed.";
-  console.log(message);
-  context.succeed(message);
-})
-.catch((messages) => {
-    if ( messages === "Object was already processed." ) {
-    console.log("Image already processed");
-    context.succeed("Image already processed");
-  } else {
-    context.fail("Error processing " + s3Object.object.key + ": " + messages);
-  }
-});
+    console.log(message);
+    context.succeed(message);
+  })
+    .catch((messages) => {
+        if ( messages === "Object was already processed." ) {
+            console.log("Image already processed");
+            context.succeed("Image already processed");
+        } else   {
+            context.fail("Error processing " + s3Object.object.key + ": " + messages);
+        }
+    });
 };
